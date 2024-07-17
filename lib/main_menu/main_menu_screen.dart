45e3cdd2e997f259ebf34b682e9bd5d8b6050918
Blake Thompson <blake.thompson93@gmail.com>
2024-07-16 22:23:42 -0700

    base game

---
+++ lib/main_menu/main_menu_screen.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/material.dart';
+
+class MainMenuScreen extends StatelessWidget {
+  const ManMenuScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.black,
+      body: LayoutBuilder(
+        builder: (context, contraints) {
+          
+        },
+        )
+    );
+  }
+}
