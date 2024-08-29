acceee3b2e965a3478a54f4bc2ac2624466da119
Blake <blake.thompson93@gmail.com>
2024-08-28 22:35:54 -0700

    fixing score spacing

--- lib/hud.dart
+++ lib/hud.dart
@@ -28,13 +28,14 @@ class Hud extends PositionComponent with HasGameReference<SpaceShiftGame> {
     final scoreCountRenderer = TextPaint(
       style: textStyle.copyWith(fontSize: 55, fontWeight: FontWeight.bold),
     );
+    print(game.size.x);
 
     add(
       TextComponent(
         text: 'SCORE',
-        position: Vector2(game.size.x - 100, 10),
+        position: Vector2(game.size.x - 40, 30),
         textRenderer: defaultRenderer,
-        anchor: Anchor.center,
+        anchor: Anchor.bottomRight,
       ),
     );
     _scoreTextComponent = TextComponent(
